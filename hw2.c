b3c9f529de3da952ba2edcf9e45c17191e1e62a5
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-09-22 13:17:11 +0900

    3주차 과제 등록

---
+++ hw2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double km, mile;
+
+	printf("Please enter kilometers: ");
+	scanf("%lf", &km);
+
+	mile = km / 1.609;
+	
+	printf("%.1lf km is equal to %.1lf miles. \n", km, mile);
+
+	return 0;
+}
